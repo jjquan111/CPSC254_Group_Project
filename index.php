cc357540d8dd1758d962bd97f761f1bab5238c83
stevenly <stevenly3030@csu.fullerton.edu>
2024-05-05 10:33:16 -0700

    added index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Eventify</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Eventify</h1>
+        <p>Click Enter to view your Events</p>
+        <div class="options">
+            <a href="dynamic_full_calendar.php">Enter</a>
+        </div>
+    </div>
+</body>
+</html>
