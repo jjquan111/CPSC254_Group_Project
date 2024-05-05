d83a454df967a7da59f2a32fadaf3f35a7c28ab4
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2024-05-05 16:36:59 -0700

    Update dynamic_full_calender.php

--- dynamic_full_calender.php
+++ dynamic_full_calender.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!--use fullcalendar css for calendar-->
+    <title>Dynamic Full Calendar</title>
+    <!-- Linking FullCalendar and Bootstrap CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css" rel="stylesheet" />
-    
-    <!--use jquery for DOM-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    
-    <!--use momentjs to format dates-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
-    
-    <!--fullcalendar library -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Linking jQuery, Popper.js, and Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Linking Moment.js and FullCalendar JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
 </head>
 <body>
     <!--calendar container-->
