3e60fc57ca44bdbd1deb10d887207d7cb71f5c24
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2024-05-05 16:57:41 -0700

    Update fetch_events.php

--- fetch_events.php
+++ fetch_events.php
@@ -10,9 +10,8 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-$query = "SELECT id, title, start_event as start, end_event as end FROM events WHERE user_id = ?";
+$query = "SELECT id, title, start_event as start, end_event as end FROM events";
 $stmt = $pdo->prepare($query);
-$stmt->execute([$_SESSION['user_id']]); 
 $events = $stmt->fetchAll(PDO::FETCH_ASSOC); //fetch all rows
 
 echo json_encode($events); //returns events
