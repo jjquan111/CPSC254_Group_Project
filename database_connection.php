9e94b165771e6611fc60eddd35b9603d15ae218a
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2024-05-05 20:39:06 -0700

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -3,7 +3,7 @@ $host = 'localhost';
 $dbname = 'calendar';
 $username = 'your_username';
 $password = 'your_password';
-
+// Connect to the database
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     // Set the PDO error mode to exception
